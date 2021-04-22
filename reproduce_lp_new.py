fe52cb2574041a0c960adcc249b98a9bfd9b152b
Caglar Demir <caglardemir8@gmail.com>
2021-04-22 17:26:08 +0200

    reformatted.

--- reproduce_lp_new.py
+++ reproduce_lp_new.py
@@ -3,12 +3,18 @@ from models.ensemble import Ensemble
 
 run_FB15K_237 = True
 run_WN18RR = True
+run_YAGO3_10 = True
 
-wn18_path = 'KGs/WN18'
 fb15k237_path = 'KGs/FB15k-237'
 yago3_10_path = 'KGs/YAGO3-10'
-fb15k_path = 'KGs/FB15k'
 wn18rr_path = 'KGs/WN18RR'
+
+if run_YAGO3_10:
+    print('###########################################{0}##################################################'.format(
+        yago3_10_path))
+    Reproduce().reproduce(model_path='PretrainedModels/YAGO3-10/ConEx', data_path="%s/" % yago3_10_path,
+                          model_name='ConEx',
+                          out_of_vocab_flag=True)
 
 if run_FB15K_237:
     print('###########################################{0}##################################################'.format(
@@ -22,11 +28,4 @@ if run_WN18RR:
     print('###########################################{0}##################################################'.format(
         wn18rr_path))
     Reproduce().reproduce(model_path='PretrainedModels/WN18RR/ConEx', data_path="%s/" % wn18rr_path, model_name='ConEx',
-                          out_of_vocab_flag=True)
-
-
-if run_YAGO3_10:
-    print('###########################################{0}##################################################'.format(
-        yago3_10_path))
-    Reproduce().reproduce(model_path='PretrainedModels/YAGO3-10/ConEx', data_path="%s/" % yago3_10_path, model_name='ConEx',
                           out_of_vocab_flag=True)
